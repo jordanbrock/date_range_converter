acd774eee791d1702999c06ff42e724bd75f9e80
Jordan Brock <jordan@brock.id.au>
2017-01-31 20:25:01 +0800

    Added support for times in reports

--- lib/date_range_converter.rb
+++ lib/date_range_converter.rb
@@ -1,37 +1,60 @@
 class DateRangeConverter
   # RE = /(\d{2,}[-,\/]\d{2}[-,\/]\d{2,}).*(\d{2,}[-,\/]\d{2}[-,\/]\d{2,})/
-  RE = /(\d{2,}[-,\/]\d{2}[-,\/]\d{2,})/
+  # RE = /(\d{2,}[-,\/]\d{2}[-,\/]\d{2,})/
 
-  def self.match_regex datestring
-    match = RE.match(datestring)
-    return match
+  VALID_FORMATS = ["%Y-%m-%d %H:%M %Z",
+                   "%Y/%m/%d %H:%M %Z",
+                   "%Y/%m/%d %H:%M",
+                   "%Y-%m-%d %H:%M",
+                   "%Y/%m/%d",
+                   "%Y-%m-%d"
+  ]
+
+  def self.convert_to_date(datestring)
+    VALID_FORMATS.each do |format|
+      date = Time.strptime(datestring, format) rescue false
+      return date if date
+    end
+    return false
   end
 
-  def self.match_to_range match
-    return Time.parse(match[1])..Time.parse(match[2])
-  end
 
-  def self.build_range range_start, range_end
-    # return range_start.beginning_of_day..range_end.end_of_day if defined?(Rails)
+  # def self.match_regex(datestring)
+  #   match = RE.match(datestring)
+  #   return match
+  # end
+
+  # def self.match_to_range(match)
+  #   return Time.parse(match[1])..Time.parse(match[2])
+  # end
+
+  def self.build_range(split_start, split_end)
+    range_start = convert_to_date(split_start)
+    range_end = convert_to_date(split_end)
+
+    return nil if range_start == false || range_end == false
+
     return range_start..range_end
   end
 
-  def self.split_datestring datestring
+  def self.split_datestring(datestring)
     datestring.split(" - ")
   end
 
-  def self.split_rangestring datestring
+  def self.split_rangestring(datestring)
     datestring.split("..").inject{ |s,e| Time.parse(s)..Time.parse(e) }
   end
 
-  def self.string_to_daterange datestring
+  def self.string_to_daterange(datestring)
     return nil if datestring.class != String
     return nil if datestring.length == 0
+
     range = split_rangestring(datestring)
     return range if range.class == Range
-    split = split_datestring datestring
-    split = split.map{|i| match_regex(i)[0]}
-    return build_range(Time.parse(split[0]), Time.parse(split[1]))
+
+    split = split_datestring(datestring)
+    # split = split.map{|i| match_regex(i)[0]}
+    return build_range(split[0], split[1])
   end
 end
 
--- test/test_date_range_converter.rb
+++ test/test_date_range_converter.rb
@@ -2,34 +2,16 @@ require 'test/unit'
 require 'date_range_converter'
 
 class TestDateRangeConverter < Test::Unit::TestCase
-  def test_regex
-    match = DateRangeConverter.match_regex "2015-12-12"
-    assert_not_nil match
-    assert_equal "2015-12-12", match[1]
+  def test_convert_to_date_with_yyyy_mm_dd_hh_mm
+    date = DateRangeConverter.convert_to_date("2016-11-11 23:23")
+    assert_equal date, Time.parse("2016-11-11 23:23")
+    assert date
   end
 
-  def test_regex_with_different_separators
-    match = DateRangeConverter.match_regex "2015/12/12"
-    assert_equal "2015/12/12", match[1]
+  def test_convert_to_date_with_yyyy_mm_dd
+    date = DateRangeConverter.convert_to_date("2015-12-12")
+    assert_equal date, Time.parse("2015-12-12")
   end
-
-  def test_regest_with_dd_mm_yyyy
-    match = DateRangeConverter.match_regex "19/12/2015"
-    assert_not_nil match
-  end
-
-  # def test_regex_with_mm_dd_yyyy
-  #   match = DateRangeConverter.match_regex "09/23/2015"
-  #   assert_equal "09/23/2015", match[1]
-  # end
-
-  # def test_convert_to_range
-  #   match = DateRangeConverter.match_regex "2015-12-12 - 2015-12-19"
-  #   range = DateRangeConverter.match_to_range match
-
-  #   assert_not_nil range
-  #   assert_equal Range, range.class
-  # end
 
   def test_string_to_daterange
     range = DateRangeConverter.string_to_daterange "2015-12-12 - 2015-12-19"
@@ -38,16 +20,22 @@ class TestDateRangeConverter < Test::Unit::TestCase
     assert_equal Time.parse("2015-12-19"), range.end
   end
 
-  def test_string_to_daterange_with_dd_mm_yyyy
-    range = DateRangeConverter.string_to_daterange "12/12/2015 - 19/12/2015"
-    assert_equal Time.parse("2015-12-12"), range.begin
-    assert_equal Time.parse("2015-12-19"), range.end
-  end
+  # def test_string_to_daterange_with_dd_mm_yyyy
+  #   range = DateRangeConverter.string_to_daterange "12/12/2015 - 19/12/2015"
+  #   assert_equal Time.parse("2015-12-12"), range.begin
+  #   assert_equal Time.parse("2015-12-19"), range.end
+  # end
 
   def test_string_to_daterange_with_range_to_s
     daterange = "2015-06-01 00:00 AWST..2016-05-31 23:59 AWST"
     range = DateRangeConverter.string_to_daterange daterange
     assert_equal Time.parse("2015-06-01 00:00 AWST"), range.begin
+  end
+
+  def test_string_to_daterange_with_times
+    daterange = "2015-06-01 03:00 AWST - 2015-06-01 05:00 AWST"
+    range = DateRangeConverter.string_to_daterange(daterange)
+    assert_equal Time.parse("2015-06-01 03:00 AWST"), range.begin
   end
 
   # def test_string_to_daterange_with_mm_dd_yyyy
